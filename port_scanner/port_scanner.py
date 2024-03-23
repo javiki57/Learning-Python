b4bc4faf9113f9cd526ea244ef55f2a49040d854
Javiki57 <javipatinador@gmail.com>
2024-03-23 20:31:18 +0100

    Inicio de port scanner en Python

---
+++ port_scanner/port_scanner.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import socket
+import argparse
+import sys
+from termcolor import colored
+
+
+def get_arguments():
+
+    parser = argparse.ArgumentParser(description='Fast TCP Port Scanner')
+    parser.add_argument("-t", "--target", dest="target", help="Victim target to scan (Ex: -t 192.168.1.1)")
+    parser.add_argument("-p", "--port", dest="port", help="Port range to scan (Ex: -p 1-100)")
+    options = parser.parse_args()
+
+    if not options.target or options.port is None:
+        parser.print_help()
+        sys.exit(1)
+    
+    return options.target, options.port
+
+def create_socket():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5) # Timpo para ver si está abierto o cerrao
+    return s
+
+def port_scanner(port, host, s):
+
+    try:
+        s.connect((host, port)) 
+        print(colored(f"\n[+] El puerto {port} está abierto", 'green'))
+        s.close()
+
+    except (socket.timeout, ConnectionRefusedError):
+        s.close()
+
+def main():
+
+    target, port = get_arguments()
+
+    if '-' in port:
+        ports = port.split('-')
+               
+        for port in range(int(ports[0]), int(ports[1])):
+            s = create_socket()
+            port_scanner(port, target, s)
+
+
+if __name__ == "__main__":
+    main()
